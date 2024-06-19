c46096957ba540d8a4283a59182fe35c1b372186
KiotoLite <elazoj@unsa.edu.pe>
2024-06-19 11:27:07 -0500

    Agrega a la rma principal

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 x = "Hola mundo "
 print(x)
-
-
+print('Hello wordl ')
+print('2+3')
+print(2+4)
+print('How are you?')
 def funcion(entrada):
     return entrada/2 
     
@@ -11,3 +13,62 @@ x = int(x)
 x = funcion(x)
 print(x)
 print(type(x))
+dividiendo = 17
+divisor = 3
+cociente = dividiendo//divisor
+print (cociente)
+cociente = dividiendo%divisor
+print(cociente)
+#Potenciacion
+print(3**2)
+#Radicacion
+print(4*0.5)
+#Valor Absoluto
+print(abs(-3))
+print(abs(12))
+print(abs(-45))
+# sumas de variables 
+print(False*23)
+print(True+23.4)
+#Como saber el tipo de varible
+print(type(dividiendo))
+print(type(divisor))
+#Redondear valores
+pi = 3.12456677888
+print(round(pi,1))
+print(round(pi,2))
+print(round(pi,3))
+print(round(pi,4))
+#base binaria
+print(0b101010)
+print(bin(100001))
+#base octal
+print(0o3245)
+print(oct(34566))
+#base hexadecimal
+print(0x52563)
+print(hex(19282))
+#Strings
+print('Mi primera cadena')
+print('Los llamados string son llamadas de cadenas ')
+#conversion de datos 
+print(str(True))
+#Salto de linea
+msg = 'Hola como estas \nI am not happy'
+print(msg)
+#tabulador
+msg = 'Primera tabla \t Segunda tabla \t Tercera tabla\t'
+print(msg)
+#comilla simple
+msg = 'Necesitamos \'escapar\' la comilla simple'
+print(msg)
+#Barra invertida 
+msg = 'Tabla \\  seccion \\ Encabezado \\'
+print(msg)
+text = 'abc\ndfg'
+print(text)
+text = ' a \t b \t c'
+print(text)
+
+text = r'a\tb\tc'
+print(text)
